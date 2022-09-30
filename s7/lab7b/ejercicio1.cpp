bbc851bc2195674112d7d5fdc461821762a109c4
jchavezautec <111583105+jchavezautec@users.noreply.github.com>
2022-09-30 08:22:53 -0500

    Update ejercicio1.cpp

--- s7/lab7b/ejercicio1.cpp
+++ s7/lab7b/ejercicio1.cpp
@@ -23,7 +23,7 @@ void bubble_sort(vector<T>& A){
 template<class T>
 void insertion_sort(vector<T>& A){
     for(std::size_t j = 1; j < A.size(); j++){
-        int key = A[j];
+        T key = A[j];
         int i = j-1;
 
         while(i >= 0 && A[i] > key){
@@ -44,7 +44,9 @@ int main(){
     srand((unsigned)time( NULL ) );
     chrono::time_point<std::chrono::system_clock> inicio, fin;
     vector<double> A(200000);
+    vector<double> B(200000);
     generate(A.begin(), A.end(), [](){return (double)rand()/RAND_MAX;});
+    B = A;
 
     inicio = chrono::high_resolution_clock::now(); // Temporizador: Inicio
     bubble_sort(A);
@@ -53,12 +55,12 @@ int main(){
     std::chrono::duration<double, std::milli> t1 = fin - inicio;
     cout << "bubble_sort(time) = " << t1.count() << "ms" << endl;
 
-    vector<double> B(200000);
-    generate(B.begin(), B.end(), [](){return (double)rand()/RAND_MAX;});
+    //is_sorted(B);
     inicio = chrono::high_resolution_clock::now(); // Temporizador: Inicio
     insertion_sort(B);
     fin    = chrono::high_resolution_clock::now(); // Temporizador: Fin
     is_sorted(B);
+
     std::chrono::duration<double, std::milli> t2 = fin - inicio;
     cout << "insertion_sort (time) = " << t2.count() << "ms" << endl;
     return 0;
