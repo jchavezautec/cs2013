8f69a382a311bf1fe4418569c679a0ea2f47112e
José Chávez <jchaveza@utec.edu.pe>
2022-10-11 20:51:35 -0500

    lab9a mod

--- s9/lab9a/ejercicio2.cpp
+++ s9/lab9a/ejercicio2.cpp
@@ -10,13 +10,13 @@ using namespace std;
 
 mutex mtx;
 
-void suma(vector<int> A, int& s, int chunk, int chunks){
+void dot(vector<int> A, vector<int> B, int& s, int chunk, int chunks){
     int inicio = chunk * (A.size()/chunks);
     int fin = (chunk + 1) * (A.size()/chunks);
     int s_ = 0;
      
     for (int i=inicio; i < fin; i++)
-        s_ += A[i];
+        s_ += A[i] * B[i];
 
     mtx.lock();
     s = s + s_;
@@ -24,9 +24,9 @@ void suma(vector<int> A, int& s, int chunk, int chunks){
 
 }   
 
-void suma_(vector<int> A, int& s){
+void dot_(vector<int> A, vector<int> B, int& s){
     for (int i=0; i < A.size(); i++)
-        s += A[i];    
+        s += A[i] * B[i];    
 }
 
 int main(){
@@ -34,26 +34,24 @@ int main(){
     int N = 10000000;
 
     vector<int> A(N, 1.);
+    vector<int> B(N, 1.);
 
     vector<thread> vthreads;
     int n_threads = 2;
 
-    //generate(A.begin(), A.end(), [](){return (double)rand()/RAND_MAX;});
-    //generate(B.begin(), B.end(), [](){return (double)rand()/RAND_MAX;});
-
     t_inicio = chrono::high_resolution_clock::now();
     
-    int res=0;
+    int res=0.;
     for (int i=0; i < n_threads; i++)
-        vthreads.push_back(thread(&suma,A,ref(res),i,n_threads));
+        vthreads.push_back(thread(&dot,A,B,ref(res),i,n_threads));
     
     for (int i=0; i < n_threads; i++)
         vthreads[i].join();
-    //suma_(A,res);
+    //dot_(A,B,res);
     t_final  = chrono::high_resolution_clock::now(); 
-    std::chrono::duration<double, std::milli> t = t_final - t_inicio;
+    std::chrono::duration<int, std::milli> t = t_final - t_inicio;
     cout << "Tiempo = " << t.count() << "ms" << endl;
-    cout << "suma_elementos(A) = " << res << endl;
+    cout << "<A . B> = " << res << endl;
     assert(res == N);
 
     return 0;
