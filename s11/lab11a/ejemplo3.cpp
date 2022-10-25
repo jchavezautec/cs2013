b5b4ca0fc9af2d4b95993f0c8be7f55ed9bcd5a1
José Chávez <jchaveza@utec.edu.pe>
2022-10-25 11:48:10 -0500

    lab11a sing

--- s11/lab11a/ejemplo3.cpp
+++ s11/lab11a/ejemplo3.cpp
@@ -6,19 +6,27 @@ class Singleton
 {
     private:
         int _data;
+
+        // La instancia unica
         static Singleton* _inst;
+
+        // Constructor privado
         Singleton(int data) {
             this->_data = data;
         }
  
     public:
+        // Este metodo permite acceder a la
+        // Instancia unica
         static Singleton* getInstance(int data);    
         int getData(){
             return _data;
         }
 };
 
+// Definir el puntero Singleton estatico
 Singleton* Singleton::_inst = NULL;
+
 Singleton* Singleton::getInstance(int data){
     if (_inst == NULL)
         _inst = new Singleton(data);
