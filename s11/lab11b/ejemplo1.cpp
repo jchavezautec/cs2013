2e407f1209b559af653887c332b29f8e2e8d3bf3
José Chávez <jchaveza@utec.edu.pe>
2022-10-25 21:02:17 -0500

    lab11b

---
+++ s11/lab11b/ejemplo1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+ 
+class Vehiculo {
+public:
+    virtual void print() = 0;
+};
+class Moto: public Vehiculo {
+public:
+    void print(){
+        cout << "Moto..." << endl;
+    }
+};
+class Carro: public Vehiculo {
+public:
+    void print(){
+        cout << "Carro..." << endl;
+    }
+};
+ 
+class Cliente {
+private:
+    Vehiculo* _inst;
+public:
+    Cliente(int tipo_vehiculo){
+        if (tipo_vehiculo == 1)
+            _inst = new Moto();
+        else if (tipo_vehiculo == 2)
+            _inst = new Carro();
+        else
+            _inst = NULL;
+    } 
+    ~Cliente(){
+        if (_inst) {
+            delete _inst;
+            _inst = NULL;
+        }
+    }
+    Vehiculo* getVehiculo() { return _inst; }
+};
+ 
+int main()
+{
+    Cliente* ptr_cliente = new Cliente(1);
+    Vehiculo* ptr_vehiculo = ptr_cliente->getVehiculo();
+    ptr_vehiculo->print();
+    return 0;
+}
