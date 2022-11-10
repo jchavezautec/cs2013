df2b694aac4579a4d294e5798a3e77de70b639c0
José Chávez <jchaveza@utec.edu.pe>
2022-11-10 00:57:30 -0500

    lab13b

--- s13/lab13b/ejercicio2.cpp
+++ s13/lab13b/ejercicio2.cpp
@@ -3,77 +3,35 @@
 using namespace std;
 
 struct Nodo{
-    int item;
-    Nodo* next;
-};
-
-class Queue{
-    private:
-        Nodo* front;
-        Nodo* back;
-    public:
-        Queue(){
-            front = NULL;
-            back = NULL;
-        }
-        bool empty(){
-            if (front == NULL)
-                return true;
-            return false;
-        }
-        void enqueue(int e){
-            Nodo* temp = new Nodo;
-            temp->item = e;
-            temp->next = NULL;
-            
-            if (back ==NULL){
-                back = temp;
-                front = temp;
-            }
-            else{
-                back->next = temp;
-                back = temp;
-            }
-        }
-
-        int dequeue(){
-            if (!empty()){
-                Nodo* temp = front;
-                int out = front->item;
-                front = front->next;
-
-                delete temp;
-                if (front == NULL)
-                    back = NULL;
-            }
-            else
-                cout << "EMPTY!" << endl;
-        }
-        void displayQueue(){
-            if (empty())
-                cout << "Queue empty!" << endl;
-            else{
-                Nodo* temp = front;
-                while (temp != NULL){
-                    cout << temp->item;
-                    temp = temp->next;
-                    if (temp == NULL)
-                        cout << " <= BACK";
-                    cout << endl;
-                }
-            }
-        }
+    int _item;
+    Nodo* _next;
 };
 
 int main(){
-    Queue myqueue;
+    // Cola vacia
+    Nodo* front = NULL;
+    Nodo* back = NULL;
 
-    myqueue.dequeue();
-    myqueue.enqueue(8);
-    myqueue.enqueue(3);
-    myqueue.enqueue(7);
-    myqueue.enqueue(2);
-    myqueue.enqueue(4);
-    myqueue.dequeue();
-    myqueue.displayQueue();
+    //Enqueue(3)
+    Nodo* temp1 = new Nodo;
+    temp1->_item = 3;
+    temp1->_next = NULL;
+
+    front = temp1;
+    back = temp1;
+
+    cout << "front = " << front->_item << endl;
+    cout << "back = " << back->_item << endl;
+
+    //Enqueue(5)
+    Nodo* temp2 = new Nodo;
+    temp2->_item = 5;
+    temp2->_next = NULL;
+
+    back->_next = temp2;
+    back = temp2;
+
+    cout << "front = " << front->_item << endl;
+    cout << "back = " << back->_item << endl;
+    cout << "next(front) = " << front->_next->_item << endl;
 }
