14efccb0013ec8a1cd25a80b6ffec60b60ac16b5
José Chávez <jchaveza@utec.edu.pe>
2023-01-12 20:52:56 -0500

    lab2b ejercicios

--- s2/lab2b/overloading/ej1.cpp
+++ s2/lab2b/overloading/ej1.cpp
@@ -18,6 +18,20 @@ public:
     return res;
   }
 
+  numComp operator-(numComp const &obj) {
+    numComp res;
+    res.real = real - obj.real;
+    res.imag = imag - obj.imag;
+    return res;
+  }
+
+  numComp operator*(numComp const &obj) {
+    numComp res;
+    res.real = (real * obj.real - imag * obj.imag);
+    res.imag = (real * obj.imag + imag * obj.real);
+    return res;
+  }
+
   void show() { 
     cout << real << " + i" << imag << '\n'; 
   }
@@ -26,13 +40,14 @@ public:
 int main() {
   numComp c1(2, 3), c2(1, 1);
   c1.show();
-  /*
-  Ejercicio 1
-    a. Crear el metodo "show"
-    b. (Suma) Sobrecarga del operador '+'
-    c. (Resta) Sobrecarga del operador '-'
-    d. (Mult.) Sobrecarga del operador '*'
-  */
+  c2.show();
+
   numComp c3 = c1 + c2;
+  numComp c4 = c1 - c2;
+  numComp c5 = c1 * c2;
+
   c3.show();
+  c4.show();
+  c5.show();
+  
 }
--- s2/lab2b/overloading/ej2.cpp
+++ s2/lab2b/overloading/ej2.cpp
@@ -1,116 +1,120 @@
 #include <iostream>
 #include <random>
 
+using namespace std;
+
 class matrix{
     public:
-        matrix(int rows, int cols);
-        matrix(const matrix &M);
-        matrix operator+(const matrix &M)const;
-
-        ~matrix();
-        int row_size();
-        int col_size();
-
-        void to_zeros();
-        void fill_random();
-
-        int operator()(int row, int col);
+        matrix(int rows, int cols);             // Conctructor
+        matrix(const matrix &M);                // Permite realizar la copia
         
-        friend std::ostream& operator<<(std::ostream& os, matrix &M);
+        matrix operator+(const matrix &M)const; // Sobrecarga al operador +
+        int operator()(int row, int col);       // Sobrecarga al operador ()
 
+        ~matrix();          // Destructor
+        int get_rows();      // filas
+        int get_cols();      // columnas
+
+        void random();      // Rellenar con numeros aleatorios
+
+        // Sobrecarga al operador <<
+        friend std::ostream& operator<<(std::ostream& os, matrix &M);
     private:
         int _rows;
         int _cols;
-        int *_m;
+        int **_m;
 };
 
 matrix::matrix(int rows, int cols){
     _rows = rows;
     _cols = cols;
-    _m = new int[rows * cols];
+    
+    _m = new int*[rows];
+    for(int i=0; i < rows; i++)
+        _m[i] = new int[cols];
+
+    this->random();
+
 }
+
 matrix::matrix(const matrix &M){
     _rows = M._rows;
     _cols = M._cols;
     
-    _m = new int[_rows * _cols];
-
-    for (int i=0; i < _rows * _cols; i++){
-        _m[i] = M._m[i];
+    _m = new int*[_rows];
+    for(int i=0; i < _rows; i++){
+        _m[i] = new int[_cols];
+        for(int j=0; j < _cols; j++)
+            _m[i][j] = M._m[i][j];
     }
 }
 
 matrix::~matrix(){
+    for(int i=0; i<_rows; i++)
+        delete[] _m[i];
     delete[] _m;
 }
 
-int matrix::row_size(){
+int matrix::get_rows(){
     return _rows;
 }
-int matrix::col_size(){
+int matrix::get_cols(){
     return _cols;
 }
 int matrix::operator()(int i, int j){
-    return _m[i * _cols + j];
+    return _m[i][j];
 }
 
-void matrix::to_zeros(){
-    for (int i=0; i < _rows; i++){
-        for (int j=0; j < _cols; j++){
-            _m[i * _cols + j] = 0;
-        }
-    }
-}
-void matrix::fill_random(){
-    std::random_device rd;
+void matrix::random(){
+    random_device rd;
 
     for (int i=0; i < _rows; i++){
         for (int j=0; j < _cols; j++){
-            _m[i * _cols + j] = static_cast<int>(rd()) % 100;
+            _m[i][j] = static_cast<int>(rd()) % 100;
         }
     }
 }
 
 std::ostream& operator<<(std::ostream& os, matrix &M){
-    for (int i=0; i < M.row_size(); i++){
-        std::cout << "| ";
-        for (int j=0; j < M.col_size(); j++){
-            std::cout.width(3);
-            std::cout << M(i,j) << " ";
+    cout << endl;
+    for (int i=0; i < M.get_rows(); i++){
+        cout << "| ";
+        for (int j=0; j < M.get_cols(); j++){
+            cout.width(4);
+            cout << M(i,j) << " ";
         }
-        std::cout.width(1);
-        std::cout << "|"<< std::endl;
+        cout.width(1);
+        cout << "|"<< endl;
     }   
 }
 matrix matrix::operator +(const matrix &M) const{
     if (_rows != M._rows || _cols != M._cols){
-        std::cout << "Las dimensiones no coinciden!" << std::endl;
+        cout << "Las dimensiones no coinciden!" << std::endl;
     }
 
     matrix res(_rows, _cols);
 
-    for (int i=0; i < _rows * _cols; i++)
-        res._m[i] = _m[i] + M._m[i]; 
+    for (int i=0; i < _rows; i++){
+        for (int j=0; j < _cols; j++){
+            res._m[i][j] = _m[i][j] + M._m[i][j];
+        }
+    }
+         
 
     return res;
 }
 
 int main(){   
-    /* Ejercicio 1:
-      a. Constructor
-      b. Sobrecarga al operador '<<'
-    */
     matrix m1(4,3);
-    m1.fill_random(); // Inicializar con valores aleatorios
-    std::cout << m1;
+    cout << m1;
   
-    // Ejercicio 2: Copy
+    // Copia
     matrix m2 = m1;
 
-    std::cout << m2;
+    cout << m2;
 
     matrix m3 = m1 + m2;
 
-    std::cout << m3;
+    cout << m3;
     return 0;
 }
