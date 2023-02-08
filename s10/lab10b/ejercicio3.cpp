228b5e7543de9aec9459c30e9a56532ccb995153
José Chávez <jchaveza@utec.edu.pe>
2023-02-08 10:47:20 -0500

    ej3 lab10b

--- s10/lab10b/ejercicio3.cpp
+++ s10/lab10b/ejercicio3.cpp
@@ -1,54 +1,51 @@
 #include <iostream>
-#include <thread>
-#include <vector>
-#include <algorithm>
 #include <chrono>
+#include <math.h>
+#include <vector>
 #include <thread>
-#include <assert.h>
 
 using namespace std;
 
-void sumar(vector<double> A, vector<double> B, double& s, int chunk, int chunks){
-    int inicio = chunk * (A.size()/chunks);
-    int fin = (chunk + 1) * (A.size()/chunks);
+bool es_primo(int n){
+    if (n == 2)
+        return true;
+    for(int d = 2; d < n; d++){
+        if (n % d == 0)
+            return false;
+    }
+    return true;
+}
 
-    for (int i=inicio; i < fin; i++)
-        s += A[i] * B[i];
-}   
+void contar_primos_w_threads(int inicio, int fin, int& count){
+    for(int i = inicio; i < fin; i++){
+        if(es_primo(i)){
+            count++;
+        }
+    }
+}
 
 int main(){
     chrono::time_point<std::chrono::system_clock> t_inicio, t_final;
-
-    vector<double> A(100000000);
-    vector<double> B(100000000);
-
+    
     vector<thread> vthreads;
-    int n_threads = thread::hardware_concurrency()/2;
-
-    generate(A.begin(), A.end(), [](){return (double)rand()/RAND_MAX;});
-    generate(B.begin(), B.end(), [](){return (double)rand()/RAND_MAX;});
-
+    int n_threads = 4;
+    int size = (pow(2,25)-2 + 1);
+    int count[4] = {0,0,0,0};
     t_inicio = chrono::high_resolution_clock::now();
     
-    vector<double> outs(n_threads,0.);
-
-    for (int i=0; i < n_threads; i++){
-        vthreads.push_back(thread(&sumar, A, B, ref(outs[i]),i,n_threads));
+    for (int i=0; i < 4; i++){
+        int inicio = 2 + i * size / 4;
+        int fin    = 2 + (i + 1) * size / 4;
+        vthreads.push_back(
+            thread(&contar_primos_w_threads, inicio, fin, ref(count[i])));
     }
 
-    
-    for (int i=0; i < n_threads; i++){
+    for (int i=0; i < 4; i++){
         vthreads[i].join();
     }
 
-    double res=0.;
-    for (int i=0; i < n_threads; i++){
-        res += outs[i];
-    }
     t_final  = chrono::high_resolution_clock::now(); 
     std::chrono::duration<double, std::milli> t = t_final - t_inicio;
     cout << "Tiempo = " << t.count() << "ms" << endl;
-               
     return 0;
 }
-
