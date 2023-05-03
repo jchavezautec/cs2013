f2935334a2d95597901e7caffc512bfe75d39ba3
Jose Armando Chavez Alvarez <jchaveza@utec.local>
2023-05-03 13:25:18 -0500

    week 5 mod

--- s5/lab5a/ejercicio1.cpp
+++ s5/lab5a/ejercicio1.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-float suma(float x1, float x2){ return x1 + x2; }
-float resta(float x1, float x2){ return x1 - x2; }
-float mult(float x1, float x2){ return x1 * x2; }
-float div(float x1, float x2){ return x1 / x2; }
-float mod(int x1, int x2){ return ((x1 % x2) + x2)%x2; }
-
-template<class T>
-void calculadora(float(*f)(T, T), T x1, T x2){
-    float resultado = f(x1,x2);
-    cout << resultado << endl;
+void bubbleSort(vector<int> &A){
+    for (int i = 0; i < A.size() - 1; i++)
+        for (int j = 0; j < A.size() - i - 1; j++)
+            if (A[j] > A[j + 1])
+                swap(A[j],A[j+1]);
 }
 
+void bubbleSortMod(vector<int> &A, bool (*comp)(int, int)){
+    for (int i = 0; i < A.size() - 1; i++)
+        for (int j = 0; j < A.size() - i - 1; j++)
+            if (comp(A[j], A[j + 1]))
+                swap(A[j],A[j+1]);
+}
+
+bool compasc(int a, int b){ return a > b;}
+bool compdes(int a, int b){ return a < b;}
+bool compabs(int a, int b){ return abs(a) > abs(b);}
+
 int main(){
-    calculadora<float>(&suma, 3, 2);
-    calculadora<float>(&resta, 3, 2);
-    calculadora<float>(&mult, 3, 2);
-    calculadora<float>(&div, 3, 2);
-    calculadora(&mod, 3, 2);
+    vector<int> A = {5, 2, -4, 6, 1, -3};
+    bubbleSort(A);
+    for_each(A.begin(), A.end(), [](int x){cout << x << " ";}); cout << endl;
+
+    vector<int> B = {5, 2, 4, 6, 1, -3};
+    bubbleSortMod(B, compasc);
+    for_each(B.begin(), B.end(), [](int x){cout << x << " ";}); cout << endl;
+
+    bubbleSortMod(B, compdes);
+    for_each(B.begin(), B.end(), [](int x){cout << x << " ";}); cout << endl;
+
+    bubbleSortMod(B, compabs);
+    for_each(B.begin(), B.end(), [](int x){cout << x << " ";}); cout << endl;
+
     return 0;
 }
 
