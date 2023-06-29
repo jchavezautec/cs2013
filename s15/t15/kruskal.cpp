d7723f16ecb673ea4a059e6b6e42136248bc0f6a
Jose Ch <jchaveza@utec.edu.pe>
2023-06-29 15:51:25 -0500

    s15 teo mod kruskal

--- s15/t15/kruskal.cpp
+++ s15/t15/kruskal.cpp
@@ -40,8 +40,8 @@ void Grafo<T>::nueva_arista(T v1, T v2, int peso){
     aristas.emplace_back(arista<T>(v1, v2, peso));
     padres[v1] = v1;
     padres[v2] = v2;
-    rango[v1] = 0;
-    rango[v2] = 0;
+    rango[v1] = 1;
+    rango[v2] = 1;
 }
 
 template <typename T>
@@ -60,7 +60,7 @@ void Grafo<T>::kruskal(){
     sort(aristas.begin(), aristas.end()); // Ordenar aristas por peso
     for (const auto& e: aristas){
         cout << "Arista: " << e.v1 << " --- " << e.v2 << endl;
-        T ancestro1 = ancestro(e.v1);
+        T ancestro1 = ancestro(e.v1);   
         T ancestro2 = ancestro(e.v2);
 
         // La arista 'e' formara un ciclo si sus vertices tienen el mismo ancestro
@@ -74,7 +74,7 @@ void Grafo<T>::kruskal(){
             else if (rango[ancestro1] < rango[ancestro2]) 
                 padres[ancestro1] = ancestro2;
             else{
-                padres[ancestro2] = ancestro1;
+                padres[ancestro2] = ancestro1; 
                 rango[ancestro1]++;
             }
         }
